d2a88063d5b2f3025406c0d7760819e74ff49d8b
Christopher <tophercrnt@gmail.com>
2024-05-17 09:58:09 +0200

    Homepage elements position

--- argentbank/src/App.jsx
+++ argentbank/src/App.jsx
@@ -4,7 +4,7 @@ import Header from "./components/Header/Header"
 
 function App() {
   return (
-    <div>
+    <div className="homepage">
       <Header />
       <main>
         <section className="home_first_section">
