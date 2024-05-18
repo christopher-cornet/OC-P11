e42d915ae417d839bb4ed7aea38f42aac21e16aa
Christopher <tophercrnt@gmail.com>
2024-05-18 16:53:37 +0200

    Change header and footer based on actual URL

--- argentbank/src/components/Footer/Footer.jsx
+++ argentbank/src/components/Footer/Footer.jsx
@@ -1,9 +1,16 @@
 import './Footer.css';
 
 function Footer() {
+  if (window.location.pathname === "/profile") {
+    return (
+      <footer style={{border: "none"}}>
+        <p>Copyright 2020 Argent Bank</p>
+      </footer>
+    );
+  }
   return (
     <footer>
-        <p>Copyright 2020 Argent Bank</p>
+      <p>Copyright 2020 Argent Bank</p>
     </footer>
   );
 }
--- argentbank/src/components/Header/Header.jsx
+++ argentbank/src/components/Header/Header.jsx
@@ -2,10 +2,11 @@ import React from "react";
 import { Link } from "react-router-dom";
 import logo from "../../images/argentBankLogo.webp";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCircleUser } from "@fortawesome/free-solid-svg-icons";
+import { faCircleUser, faGear, faPowerOff } from "@fortawesome/free-solid-svg-icons";
 import "./Header.css";
 
 function Header() {
+  if (window.location.pathname === "/profile") {
     return (
       <header>
           <Link to="/">
@@ -13,14 +14,32 @@ function Header() {
                 <img src={logo} className="logo" alt="Accueil du site Argent Bank" />
             </section>
           </Link>
-          <Link to="/login">
-            <section className="login">
-                <FontAwesomeIcon icon={faCircleUser} className="user-icon" size="lg" style={{color: "#2c3e50",}} />
-                <p className="sign-in">Log In</p>
-            </section>
-          </Link>
+          <section className="login">
+              <p className="sign-in" style={{marginRight: "10px", color: "#61b37b", fontWeight: "600"}}>Name</p>
+              <FontAwesomeIcon icon={faCircleUser} className="user-icon" size="lg" style={{marginRight: "20px", color: "#61b37b", height: "25px"}} />
+              <FontAwesomeIcon icon={faGear} size="lg" style={{marginRight: "20px", color: "#61b37b", height: "25px"}} />
+              <Link to="/login">
+                <FontAwesomeIcon icon={faPowerOff} size="lg" style={{color: "#61b37b", height: "25px"}} />
+              </Link>
+          </section>
       </header>
     );
   }
+  return (
+    <header>
+        <Link to="/">
+          <section>
+              <img src={logo} className="logo" alt="Accueil du site Argent Bank" />
+          </section>
+        </Link>
+        <Link to="/login">
+          <section className="login">
+              <FontAwesomeIcon icon={faCircleUser} className="user-icon" size="lg" style={{color: "#2c3e50",}} />
+              <p className="sign-in">Log In</p>
+          </section>
+        </Link>
+    </header>
+  );
+}
   
-  export default Header;
+export default Header;
